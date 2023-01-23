7780b1dc67e0e45bf5f21d74fc0cf9c66ebbf595
[Andrej Ščiupakov] <[sciupakov@gmail.com]>
2023-01-23 13:16:09 +0200

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Agreement",
+    'version': '16.0',
+    'depends': ['base'],
+    'author': "Andrej Ščiupakov",
+    'category': 'Category',
+    'license': 'GPL-3',
+    'description': """
+    Test task module
+    """,
+    'data': [
+    ],
+    'demo': [
+    ],
+}
